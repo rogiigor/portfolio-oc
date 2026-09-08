8b6af41455e33f8450bfa28fc24c1f918a3d8f73
Igor Vish <igorv0@yahoo.com>
2026-09-08 08:21:18 -0500

    added distribution

---
+++ src/data/projectsData.jsx
@@ -0,0 +1,8 @@
+export const projectsData = [
+    {
+        title: 'Kasa',
+        name: 'Create a Property Rental Web Application with React',
+        description: ['Skills used: ', 'React', 'Sass', 'CSS Animation'],
+
+    }
+]
