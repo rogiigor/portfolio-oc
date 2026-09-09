882914bb752fedbf5c2b6549efb66e7021b6a451
Igor Vish <igorv0@yahoo.com>
2026-09-08 21:07:40 -0500

    Trying to fix blank page

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,5 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
-  base: '/https://github.com/rogiigor/'
+  base: '/https://github.com/rogiigor/portfolio-oc/'
 })
