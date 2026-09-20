bcde7e59a72f811030fa37b57e191c917e656e2e
Igor Vish <igorv0@yahoo.com>
2026-09-20 05:06:24 -0500

    responsive design

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -10,32 +10,52 @@ const Contact = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        console.log({name, email, textarea});
+        sendEmail(name, email, textarea);
+        clearForm()
+    }
 
+    const sendEmail = (name, email, textarea) => {
+        let myEmail = "igorv0@yahoo.com"
+        let subject = `contact about web project from ${name}`
+        let body = `Salut, Je suis ${name}. Mon email est ${email}. \n\n${textarea}`
+
+        let encodedSubject = encodeURIComponent(subject)
+        let encodedBody = encodeURIComponent(body)
+        let mailto = `mailto:${myEmail}?subject=${encodedSubject}&body=${encodedBody}`
+
+
+        window.location.href = mailto
+    }
+
+    const clearForm = () => {
         setName('')
         setEmail('')
         setTextarea('')
     }
+
     return (
         <section className='contact' id="contact">
 		    <h2 className='contact-title'>Contact</h2>
-		    <form className='contact-form' onSubmit={handleSubmit} action="#" method="post">
+
+		    <form className='contact-form' onSubmit={handleSubmit}>
 			    <label className='contact-label' htmlFor="name">Name</label>
-			    <input className='contact-input' type="text" name="name" value={name} id="name" 
+			    <input className='contact-input' type="text" name="name" value={name} id="name" required
                     onChange={(e) => setName(e.target.value)} />
 
 			    <label className='contact-label' htmlFor="email">Email</label>
-			    <input className='contact-input' type="email" name="email" value={email} id="email" 
+			    <input className='contact-input' type="email" name="email" value={email} id="email" required
                     onChange={(e) => setEmail(e.target.value)}/>
 
 			    <label className='contact-label'  htmlFor="message">Message</label>
-			    <textarea className='contact-textarea' name="message" value={textarea} id="message" cols="30" rows="10"
+			    <textarea className='contact-textarea' name="message" value={textarea} id="message" cols="30" rows="10" required
                     onChange={(e) => setTextarea(e.target.value)}></textarea>
 
 			    <input className='contact-input contact-input__submit' type="submit" value="Send" />
 		    </form>
 	    </section>
     )
-} 
+}
+
+
 
 export default Contact
